46889d553971f11ded629b21ff8d4b52008dbd10
Jeff Bumgardner <bumgardner.jeffrey@gmail.com>
2016-10-18 22:46:42 -0400

    function created. formatting still off

--- js/global.js
+++ js/global.js
@@ -1,22 +1,80 @@
-// var container = document.createElement('div')
-//
-// container.classList.add('container')
-// var row = document.createElement('div')
-// row.classList.add('row')
-// container.appendChild(row)
-//
-// var col = document.createElement('div')
-// col.classList.add('col-sm-3')
-// var col2 = col.cloneNode(true)//now that you've clone the node, clone the outer shell of the node too, not just the children.
-// var col3 = col.cloneNode(true)
-// var col4 = col.cloneNode(true)
-// col.innerHTML = '<img src=http://unsplash.it/100?image=1 />'
-// col2.innerHTML = '<img src=http://unsplash.it/100?image=2 />'
-// col3.innerHTML = '<img src=http://unsplash.it/100?image=3 />'
-// col4.innerHTML = '<img src=http://unsplash.it/100?image=4 />'
-// row.appendChild(col)
-// row.appendChild(col2)
-// row.appendChild(col3)
-// row.appendChild(col4)
-//
-// document.body.appendChild(container)
+function createResultCard(item) {
+    var card = document.createElement('div')
+    card.classList.add('col-sm-3')
+
+    var img = document.createElement('img')
+    img.setAttribute('src', item.image)
+    card.appendChild(img)
+
+    var span = document.createElement('span')
+    span.innerHTML = item.title
+    card.appendChild(span)
+
+    document.querySelector('#searchResults.row').appendChild(card)
+}
+createResultCard({
+    image: 'http://unsplash.it/200?random',
+    title: 'Vintage Board Game Art Wall Home Decor',
+    seller: 'franz66',
+    price: 15.00
+})
+createResultCard({
+    image: 'http://unsplash.it/200?random',
+    title: 'Vintage Board Game Art Wall Home Decor',
+    seller: 'franz66',
+    price: 15.00
+})
+createResultCard({
+    image: 'http://unsplash.it/200?random',
+    title: 'Vintage Board Game Art Wall Home Decor',
+    seller: 'franz66',
+    price: 15.00
+})
+createResultCard({
+    image: 'http://unsplash.it/200?random',
+    title: 'Vintage Board Game Art Wall Home Decor',
+    seller: 'franz66',
+    price: 15.00
+})
+createResultCard({
+    image: 'http://unsplash.it/200?random',
+    title: 'Vintage Board Game Art Wall Home Decor',
+    seller: 'franz66',
+    price: 15.00
+})
+createResultCard({
+    image: 'http://unsplash.it/200?random',
+    title: 'Vintage Board Game Art Wall Home Decor',
+    seller: 'franz66',
+    price: 15.00
+})
+createResultCard({
+    image: 'http://unsplash.it/200?random',
+    title: 'Vintage Board Game Art Wall Home Decor',
+    seller: 'franz66',
+    price: 15.00
+})
+createResultCard({
+    image: 'http://unsplash.it/200?random',
+    title: 'Vintage Board Game Art Wall Home Decor',
+    seller: 'franz66',
+    price: 15.00
+})
+createResultCard({
+    image: 'http://unsplash.it/200?random',
+    title: 'Vintage Board Game Art Wall Home Decor',
+    seller: 'franz66',
+    price: 15.00
+})
+createResultCard({
+    image: 'http://unsplash.it/200?random',
+    title: 'Vintage Board Game Art Wall Home Decor',
+    seller: 'franz66',
+    price: 15.00
+})
+createResultCard({
+    image: 'http://unsplash.it/200?random',
+    title: 'Vintage Board Game Art Wall Home Decor',
+    seller: 'franz66',
+    price: 15.00
+})
